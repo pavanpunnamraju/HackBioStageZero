2f764c742d11fa94dafbb4e8d4b790825e47ef5d
pavanpunnamraju <pavanpunnamraju@gmail.com>
2025-11-04 20:52:32 -0800

    Add team information and favorite gene details
    This script stores team member information along with their favorite gene and DNA sequence, and prints it out.

---
+++ simple_code.py
@@ -0,0 +1,65 @@
+# Python Script: Team Information + Favorite Gene DNA Sequence
+team = [
+    {
+        "name": "Sweeta Selvan",
+        "slack_username": "@SWEETA SELVAN",
+        "country": "India",
+        "hobby": "Watching films",
+        "affiliation": "Student (Anna University, Chennai), UAA",
+        "favorite_gene": {
+            "gene_name": "TP53",
+            "dna_sequence": "ATGGAGGAGCCGCAGTCAGATCCTAGCGTCGAGCCCCCTCTGAGTCAGGAAACATTTTCAGACCTATGGAAACTACTTCCTGAAAACA"
+        }},
+    {
+        "name": "Aditya Dash",
+        "slack_username": "@aditya dash",
+        "country": "India",
+        "hobby": "Playing cricket",
+        "affiliation": "Student (IIT Bhubaneswar)",
+        "favorite_gene": {
+            "gene_name": "BRCA1",
+            "dna_sequence": "ATGGAAGTTGTCATTTTATAAAAAGGTTGCAAAATAATGTGGAAGAAAAAAAGCAGTGAACCTGTTGGTGATTTTGAAATGAGGAGGA"
+        }},
+    {
+        "name": "Gloria Omogwigho Akpederi",
+        "slack_username": "@Gloria Akpederi ",
+        "country": "Nigeria",
+        "hobby": "Reading",
+        "affiliation": "Student (University of Lagos)",
+        "favorite_gene": {
+            "gene_name": "ACE2",
+            "dna_sequence": "ATGGAGAGCCCTGTTCACATCTCCTCCTGCTCTCCTGCTCCTGCTCCTGCTCCTGGAGGAGGAGATCTTCCTCCTGCTGCTCTGCCT"
+        }},
+    {
+        "name": "Adarsh",
+        "slack_username": " @Adarsh",
+        "country": "India",
+        "hobby": "singing",
+        "affiliation": "Student (Sastra University,Thanjavur ), GUA",
+        "favorite_gene": {
+            "gene_name": "cd33",
+            "dna_sequence": "ATGGCAAACACTGCTGAAGC"
+        }},
+
+  { 
+      "name": "Pavan",
+      "slack_username": "@Pavan",
+      "country": "United States",
+      "hobby": "Basketball",
+      "affiliation": "Student (UCSD), Team Valine",
+      "favorite_gene": {
+          "gene_name": "CFTR",
+          "dna_sequence": "ATGGTGTTCAGCTGCTGCCACCTACAGCCAGTGGTGCCAGCGCTGTCACCCTCGGGGGGCCAGCTGCAGGAGGGCGAGGAGCTCGGGGCGGGGCCCTGCCCTGCGGCGGCCAGCCCTGGCAGCTGTGAGGTTGCCCGGCGCACGCCGCGGCAGAGGGGCA"
+      }
+  }
+]
+# Print the information
+for member in team:
+    print(f"Name: {member['name']}")
+    print(f"Slack Username: {member['slack_username']}")
+    print(f"Country: {member['country']}")
+    print(f"Hobby: {member['hobby']}")
+    print(f"Affiliation: {member['affiliation']}")
+    print(f"Favorite Gene: {member['favorite_gene']['gene_name']}")
+    print(f"DNA Sequence: {member['favorite_gene']['dna_sequence']}")
+    print("-" * 20)
